4e0a29783be9d8e3d977c98efde707f628ac8ca8
Chris Limbrick <climbrick1989@gmail.com>
2017-05-15 19:02:35 -0400

    finish lecture 140

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+
+	public Slider volumeSlider;
+	public Slider difficultySlider;
+	public LevelManager levelManager;
+
+	private MusicManager musicManager;
+
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+		volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+		difficultySlider.value = PlayerPrefsManager.GetDifficulty();
+	}
+
+	public void SetDefaults() {
+		volumeSlider.value = 0.8f;
+		difficultySlider.value = 2f;
+	}
+	void Update () {
+		musicManager.SetVolume (volumeSlider.value);
+	}
+
+	public void SaveAndExit() {
+		PlayerPrefsManager.SetMasterVolume (volumeSlider.value);
+		PlayerPrefsManager.SetDifficulty (difficultySlider.value);
+		levelManager.LoadLevel ("01a Start");
+	}
+}
