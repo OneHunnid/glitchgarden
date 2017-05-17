9b28b7e1dace0e501830ecf18eca000c85c24b1e
Chris <climbrick1989@gmail.com>
2017-05-17 13:07:24 -0400

    Finish lecture 163

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-	void OnTriggerEnter2D () {
-		Debug.Log (name + " trigger enter");
-	}
+
 }
